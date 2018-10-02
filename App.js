a35f3f2733a4e31959c4a83a1a6a4624443fa701
period4b <period4b@001-540-sdxx-9.guhsd.gtec>
2018-10-02 10:41:08 -0700

    I think we are going with swiper

--- App.js
+++ App.js
@@ -1,8 +1,11 @@
-//React Default Imports
+//React Imports
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-//React Addons Imports
-import { createStackNavigator } from 'react-navigation';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
 import Swiper from 'react-native-swiper';
 //Project Activities
 //import HomeScreen from './activities/Home/home'
@@ -12,22 +15,45 @@ import Swiper from 'react-native-swiper';
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.wrapper}>
-        <Text style={styles.text}>This aint it chief</Text>
-
-      </View>  
+      <Swiper style={styles.wrapper} showsButtons={false} showsPagination={false}>
+        <View style={styles.slide1}>
+          <Text style={styles.text}>Home</Text>
+        </View>
+        <View style={styles.slide2}>
+          <Text style={styles.text}>Grades</Text>
+        </View>
+        <View style={styles.slide3}>
+          <Text style={styles.text}>Schedule</Text>
+        </View>
+      </Swiper>  
     );
   }
 }
 
+
 const styles = StyleSheet.create({
   wrapper: {
+  },
+  slide1: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: '#9DD6EB',
+  },
+  slide2: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#97CAE5',
+  },
+  slide3: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#92BBD9',
   },
   text: {
-    color: '#000',
+    color: '#fff',
     fontSize: 30,
     fontWeight: 'bold',
   }
