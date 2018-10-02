8e9c59b8efabd77bd68355480d0c9a663bab29f8
cosmechz <caneko@live.com>
2018-10-01 18:48:31 -0700

    some nice changes

--- App.js
+++ App.js
@@ -1,29 +1,15 @@
 //React Utils
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-//React Addons
-import { createStackNavigator } from 'react-navigation';
 //Project Activities
 import HomeScreen from './activities/Home/home'
 import GradesScreen from './activities/Grades/grades';
-
-
-
-const RootStack = createStackNavigator(
-  {
-    Home: HomeScreen,
-    Grades: GradesScreen,
-  },
-  {
-    initialRouteName: 'Home',
-  }
-);
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <RootStack style={styles.container}/>
+        <Text>Hello, World</Text>
       </View>
     );
   }
--- README.md
+++ README.md
@@ -5,3 +5,9 @@
 -To start do expo start (needs sudo)
 
 
+##Installed Dependencies
+-NPM
+    -React Native Base
+    -React Native Swipable
+    -React Native Navigation 2.0
+Run `NPM install` if error shows up
