50517433547a7a086b544546f29339386d9911ca
cosmechz <caneko@live.com>
2018-10-01 20:45:37 -0700

    nevermind

--- App.js
+++ App.js
@@ -2,22 +2,54 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 //React Addons Imports
+import { createStackNavigator } from 'react-navigation';
 import Swiper from 'react-native-swiper';
 //Project Activities
-import HomeScreen from './activities/Home/home'
-import GradesScreen from './activities/Grades/grades';
-import CalendarScreen from './activities/Calendar/calendar'
+//import HomeScreen from './activities/Home/home'
+//import GradesScreen from './activities/Grades/grades';
+//import CalendarScreen from './activities/Calendar/calendar';
+
+class HomeScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Home Screen</Text>
+      </View>
+    );
+  }
+}
+
+class DetailsScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Details Screen</Text>
+      </View>
+    );
+  }
+}
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.wrapper}>
-        <Text style={styles.text}>Hello World</Text>
+        <RootStack></RootStack>
 
       </View>  
     );
   }
 }
+
+const RootStack = createStackNavigator(
+  {
+    Home: HomeScreen,
+    Details: DetailsScreen,
+  },
+  {
+    initialRouteName: 'Home',
+  }
+);
+
 
 const styles = StyleSheet.create({
   wrapper: {
