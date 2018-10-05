4b115cc9b6f6dbd9ae92fb8992a3b809e3146449
cosmechz <caneko@live.com>
2018-10-04 20:26:27 -0700

    Revert "this is it chief"
    This reverts commit e85daa698f70005beec63a49f6c5ab28e062d85e.

--- App.js
+++ App.js
@@ -6,22 +6,61 @@ import {
   Text,
   View
 } from 'react-native';
-import Content from './activities/content'
-
+import Swiper from 'react-native-swiper';
+//Project Activities
+//import HomeScreen from './activities/Home/home'
+//import GradesScreen from './activities/Grades/grades';
+//import CalendarScreen from './activities/Calendar/calendar';
 
 export default class App extends React.Component {
   render() {
     return (
+<<<<<<< HEAD
       <View style={styles.default}>
         <Content/>
       </View>
+=======
+      <Swiper style={styles.wrapper} showsButtons={false} showsPagination={false}>
+        <View style={styles.slide1}>
+          <Text style={styles.text}>Home</Text>
+        </View>
+        <View style={styles.slide2}>
+          <Text style={styles.text}>Grades</Text>
+        </View>
+        <View style={styles.slide3}>
+          <Text style={styles.text}>Schedule</Text>
+        </View>
+      </Swiper>  
+>>>>>>> parent of e85daa6... this is it chief
     );
   }
 }
 
 
 const styles = StyleSheet.create({
-  default: {
-    flex:1
+  wrapper: {
+  },
+  slide1: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#9DD6EB',
+  },
+  slide2: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#97CAE5',
+  },
+  slide3: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#92BBD9',
+  },
+  text: {
+    color: '#fff',
+    fontSize: 30,
+    fontWeight: 'bold',
   }
 })
