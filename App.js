42f7701e766764d7dea7ba0f0493fc3dbc21ca24
cosmechz <caneko@live.com>
2018-10-04 20:44:12 -0700

    working it chief

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ import {
   Text,
   View
 } from 'react-native';
-import Content from './activities/Content/content'
+import Content from './activities/content'
 
 
 
@@ -22,29 +22,7 @@ export default class App extends React.Component {
 
 
 const styles = StyleSheet.create({
-  wrapper: {
-  },
-  slide1: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#9DD6EB',
-  },
-  slide2: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#97CAE5',
-  },
-  slide3: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#92BBD9',
-  },
-  text: {
-    color: '#fff',
-    fontSize: 30,
-    fontWeight: 'bold',
+  default: {
+    flex:1
   }
 })
