ade451771430c81afd38c693fc29a772735097eb
Ben Berk <berk.benjamin@gmail.com>
2018-11-30 11:13:27 -0800

    Start of a planner

--- activities/Planner/planner.js
+++ activities/Planner/planner.js
@@ -1,14 +1,32 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Alert } from 'react-native';
 import { createStackNavigator } from 'react-navigation';
-
-
+import { CheckBox } from 'react-native-elements';
 
 export default class PlannerScreen extends React.Component {
+  constructor(props){
+    super(props);
+ 
+    this.state = {
+       checked: false,
+    }
+ }
   render() {
     return (
       <View>
         <Text style={styles.Title}>{"\n"}Planner</Text>
+        <CheckBox
+          style={styles.Checkbox}
+          center
+          title='Test'
+          checkedIcon='dot-circle-o'
+          uncheckedIcon='circle-o'
+          checked={this.state.checked}
+          onPress={() => {
+            this.setState({checked: !this.state.checked});
+          }}
+        />
+        
       </View>
     );
   }
@@ -31,5 +49,7 @@ const styles = {
     borderBottomWidth: 3,
     borderBottomColor: '#FFF',
   },
+  Checkbox: {
+    
+  }
 }
-
