77495478176c347d5aa8629b39a4ddfa9ef229df
Parker George <parkergeorge30@yahoo.com>
2018-12-13 10:56:00 -0800

    login test

--- activities/login/login.js
+++ activities/login/login.js
@@ -1,85 +1,85 @@
-import React from 'react';
-import { View, Text, Image } from 'react-native';
-import { GoogleSignin, GoogleSigninButton, statusCodes } from 'react-native-google-signin';
+import React from "react"
+import { StyleSheet, Text, View, Image, Button } from "react-native"
+import Expo from "expo"
 
-
-
-export default class loginScreen extends React.Component{
-    render() {
-        <GoogleSigninButton
-          style={{ width: 48, height: 48 }}
-          size={GoogleSigninButton.Size.Icon}
-          color={GoogleSigninButton.Color.Dark}
-          onPress={this._signIn}
-          disabled={this.state.isSigninInProgress} />
+export default class Login extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      signedIn: false,
+      name: "",
+      photoUrl: ""
     }
-}
-
-GoogleSignin.configure({
-    scopes: ['https://www.googleapis.com/auth/drive.readonly'], // what API you want to access on behalf of the user, default is email and profile
-    webClientId: 'AIzaSyC22xX94AZlwyQ4EK5fvqE1MP-Eyd_87lg', // client ID of type WEB for your server (needed to verify user ID and offline access)
-    offlineAccess: true, // if you want to access Google API on behalf of the user FROM YOUR SERVER
-    hostedDomain: '', // specifies a hosted domain restriction
-    loginHint: '', // [iOS] The user's ID, or email address, to be prefilled in the authentication UI if possible. [See docs here](https://developers.google.com/identity/sign-in/ios/api/interface_g_i_d_sign_in.html#a0a68c7504c31ab0b728432565f6e33fd)
-    forceConsentPrompt: true, // [Android] if you want to show the authorization prompt at each login.
-    accountName: '', // [Android] specifies an account name on the device that should be used
-    iosClientId: 'AIzaSyC22xX94AZlwyQ4EK5fvqE1MP-Eyd_87lg', // [iOS] optional, if you want to specify the client ID of type iOS (otherwise, it is taken from GoogleService-Info.plist)
-  });
-
+  }
   signIn = async () => {
     try {
-      await GoogleSignin.hasPlayServices();
-      const userInfo = await GoogleSignin.signIn();
-      this.setState({ userInfo });
-    } catch (error) {
-      if (error.code === statusCodes.SIGN_IN_CANCELLED) {
-        // user cancelled the login flow
-      } else if (error.code === statusCodes.IN_PROGRESS) {
-        // operation (f.e. sign in) is in progress already
-      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
-        // play services not available or outdated
+      const result = await Expo.Google.logInAsync({
+        androidClientId:
+          "279541889821-0jq9s32brq52pv5u9amblv8qpah7d94u.apps.googleusercontent.com",
+        //iosClientId: YOUR_CLIENT_ID_HERE,  <-- if you use iOS
+        scopes: ["profile", "email"]
+      })
+
+      if (result.type === "success") {
+        this.setState({
+          signedIn: true,
+          name: result.user.name,
+          photoUrl: result.user.photoUrl
+        })
       } else {
-        // some other error happened
+        console.log("cancelled")
       }
+    } catch (e) {
+      console.log("error", e)
     }
-  };
-  getCurrentUserInfo = async () => {
-    try {
-      const userInfo = await GoogleSignin.signInSilently();
-      this.setState({ userInfo });
-    } catch (error) {
-      if (error.code === statusCodes.SIGN_IN_REQUIRED) {
-        // user has not signed in yet
-      } else {
-        // some other error
-      }
-    }
-  };
-  isSignedIn = async () => {
-    const isSignedIn = await GoogleSignin.isSignedIn();
-    this.setState({ isLoginScreenPresented: !isSignedIn });
-  };
-  signOut = async () => {
-    try {
-      await GoogleSignin.revokeAccess();
-      await GoogleSignin.signOut();
-      this.setState({ user: null }); // Remember to remove the user from your app's state as well
-    } catch (error) {
-      console.error(error);
-    }
-  };
-  revokeAccess = async () => {
-    try {
-      await GoogleSignin.revokeAccess();
-      console.log('deleted');
-    } catch (error) {
-      console.error(error);
-    }
-  };
-  try {
-    GoogleSignin.hasPlayServices({ showPlayServicesUpdateDialog: true });
-    // google services are available
-  } catch (err) {
-    console.error('play services are not available');
-  } 
-  
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        {this.state.signedIn ? (
+          <LoggedInPage name={this.state.name} photoUrl={this.state.photoUrl} />
+        ) : (
+          <LoginPage signIn={this.signIn} />
+        )}
+      </View>
+    )
+  }
+}
+
+const LoginPage = props => {
+  return (
+    <View>
+      <Text style={styles.header}>Sign In With Google</Text>
+      <Button title="Sign in with Google" onPress={() => props.signIn()} />
+    </View>
+  )
+}
+
+const LoggedInPage = props => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.header}>Welcome:{props.name}</Text>
+      <Image style={styles.image} source={{ uri: props.photoUrl }} />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  header: {
+    fontSize: 25
+  },
+  image: {
+    marginTop: 15,
+    width: 150,
+    height: 150,
+    borderColor: "rgba(0,0,0,0.2)",
+    borderWidth: 3,
+    borderRadius: 150
+  }
+})
