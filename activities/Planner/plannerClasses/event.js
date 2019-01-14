c6501f1a27b05b3a8aa6a6186a8d476d1bcdeece
Ben <berk.benjamin@gmail.com>
2019-01-13 18:16:34 -0800

    Start of the imput

--- activities/Planner/plannerClasses/event.js
+++ activities/Planner/plannerClasses/event.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import {Text, View} from 'react-native';
+import {Input} from 'react-native-elements';
+
+export default class PlannerInput extends React.Component {
+    render() {
+
+        return(
+            <View>
+                <Text>Hello</Text>
+            </View>
+        )
+    }
+}
