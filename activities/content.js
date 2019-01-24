477770585dfebeb1da8ccc183a379736df42aaf9
Jared Banawa <jaredbanawa@gmail.com>
2019-01-24 11:09:51 -0800

    update

--- activities/content.js
+++ activities/content.js
@@ -57,7 +57,7 @@ const styles = StyleSheet.create({
     slide: {
         flex: 1,
         //justifyContent: 'center',
-        alignItems: 'center',
+        //alignItems: 'center',
         backgroundColor: '#3fa2d3',
     },
     text: {
