75b415b9c46974751d66c6805e9866e9be751eed
Jared Banawa <jaredbanawa@gmail.com>
2019-01-25 11:13:47 -0800

    Update

--- activities/Schedule/schedule.js
+++ activities/Schedule/schedule.js
@@ -10,12 +10,17 @@ export default class ScheduleScreen extends React.Component {
     return (
       <View style={{
         flex: 1,
-        flexDirection: 'column',
-        justifyContent: 'center',
+        //flexDirection: 'column',
+        justifyContent: 'space-between',
+
       }}>
-        <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
-        <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
-        <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
+          <Text style={styles.Title}>{"\n"}Schedule</Text>
+          <Text style={styles2.border}>{"\n"} Period 1: </Text>
+          <Text style={styles2.border}>{"\n"} Period 2: </Text>
+          <Text style={styles2.border}>{"\n"} Period 3: </Text>
+          <Text style={styles2.border}>{"\n"} Period 4: </Text>
+          <Text style={styles2.border}>{"\n"} Period 5: </Text>
+          <Text style={styles2.border}>{"\n"} Period 6: </Text>
       </View>
         
     );
@@ -23,11 +28,24 @@ export default class ScheduleScreen extends React.Component {
 }
 
 
+const styles2 = {
+  border: {
+          flex: .3,
+          color: '#fff',
+          fontWeight: 'bold',
+          fontSize: 20,
+          borderColor: '#fff',
+          borderWidth: 1
+          
+  }
+}
 
 const styles = {
   Title: {
     color: '#fff',
     fontSize: 30,
     fontWeight: 'bold',
+    //borderBottomWidth: 3,
+   // borderBottomColor: '#FFF',
   },
 }
